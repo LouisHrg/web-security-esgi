7ec6cead656317e9a68cb4ae7f57c10e2ad709f6
Louis <vundaboy@gmail.com>
2021-03-22 12:27:03 +0100

    better captcha project

--- recaptcha/index.php
+++ recaptcha/index.php
@@ -4,22 +4,18 @@ require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/helpers/Tooling.php";
 
 
+$app = new Leaf\App;
 $app->blade;
 $blade = new Leaf\Blade();
-$app = new Leaf\App;
 
-$app->post("/example", function() use($app) {
+$blade->configure("views", "views/cache");
 
-  $firstname = $app->request()->get('firstname');
-  $lastname = $app->request()->get('lastname');
+$app->get("/", function() use($app, $blade) {
+  echo $blade->make('index')->render();
+});
 
-  if($firstname && $lastname)
-  {
-    return $app->response()->json(['message' => 'ok !']);
-  }
-
-  return $app->response->throwErr('Erreur', 400);
-
+$app->post("/save", function() use($app) {
+  //
 });
 
 
